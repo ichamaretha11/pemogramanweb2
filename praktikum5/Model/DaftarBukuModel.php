76357effac9fe1e5916b9154e692eeb6f76e92b2
ichamaretha11 <ichamaretha55@gmail.com>
2024-10-17 12:30:40 +0700

    praktikum5

---
+++ praktikum5/Model/DaftarBukuModel.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "Buku.php";
+
+class DaftarBukuModel{
+
+public function getData (){
+    $daftar_buku = array(
+        new Buku('Belajar Pemograman Web', 'Robert T.', 'Informatika', '2024'),
+        new Buku('Matematka Diskrit', 'Rinaldi M.', 'Andi Publisher', '2017'),
+        new Buku('Kalkulus', 'Emely S.', 'Airlangga', '2024'),
+        new Buku('Metodologi Penelitian', 'James W.', 'UIN Publisher', '2018'),
+    );
+return $daftar_buku;
+
+}
+
+public function getKolomTabel(){
+    $kolom_tabel = array(
+        'No', 'Judul', 'Pengarang', 'Penerbit', 'Tahun'
+    );
+
+    return $kolom_tabel;
+}
+    //database: perpustakaan
+    //table: buku
+    //       id int(11) primary key auto_increment,
+    //       judul varchar (150),
+    //       pengarang varchar(150),
+    //       penerbit varchar(150),
+    //       tahun int (5)
+
+}
